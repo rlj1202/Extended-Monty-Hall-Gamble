6fcf23dee07e59b3a286907a54ed9a66e5aa3477
Jisu Sim <rlj1202@gmail.com>
2022-11-30 13:47:24 +0900

    Fix jsx typos

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -2,33 +2,38 @@ import { useWeb3React } from "@web3-react/core";
 import { injected } from "../lib/connectors";
 import { isNoEthereumObject } from "../lib/errors";
 
-
 import "./Login.css";
 
 export default function Login() {
-    const { chainId, account, active, activate, deactivate } = useWeb3React();
-    const handleConnect = () => {
-        if (active) {
-            deactivate();
-            return;
-        }
-        activate(injected, (error) => {
-            if (isNoEthereumObject(error))
-                window.open("https://metamask.io/download.html");
-        });
-    };
-    return (
-        <div>
-            <nav class="navbar navbar-dark bg-dark">
-                <div class="container">
-                    <a class="navbar-brand" href="#">Extended Monty-Hall Gamble</a>
-                </div>
-            </nav>
-            <div className="Login">
-                <button class="btn btn-primary" type="Login" onClick={handleConnect}>
-                    {active ? "Logout" : "Login"}
-                </button>
-            </div>
+  const { chainId, account, active, activate, deactivate } = useWeb3React();
+  const handleConnect = () => {
+    if (active) {
+      deactivate();
+      return;
+    }
+    activate(injected, (error) => {
+      if (isNoEthereumObject(error))
+        window.open("https://metamask.io/download.html");
+    });
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Extended Monty-Hall Gamble
+          </a>
         </div>
-    );
+      </nav>
+      <div className="Login">
+        <button
+          className="btn btn-primary"
+          type="Login"
+          onClick={handleConnect}
+        >
+          {active ? "Logout" : "Login"}
+        </button>
+      </div>
+    </div>
+  );
 }
